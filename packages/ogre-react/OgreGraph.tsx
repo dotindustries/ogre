71f7cb650c7b3143e5661e0ddd184423ef637aca
János Veres <janos@dot.industries>
2024-04-07 11:03:55 +0200

    feat: allow for comment options

--- packages/ogre-react/OgreGraph.tsx
+++ packages/ogre-react/OgreGraph.tsx
@@ -1,24 +1,57 @@
 import React, { useEffect, useState } from "react";
 
 import { formatGit2Json, RepositoryObject } from "@dotinc/ogre";
-import { Gitgraph } from "@gitgraph/react";
-import { GitgraphOptions } from "@gitgraph/core/src/gitgraph";
+import {
+  type CommitOptions,
+  Gitgraph,
+  type TemplateName,
+  type Orientation,
+  type Mode,
+} from "@gitgraph/react";
+import type { Template } from "@gitgraph/core/lib/template";
+
+export {
+  templateExtend,
+  metroTemplate,
+  blackArrowTemplate,
+} from "@gitgraph/core/lib/template";
+
+interface GitgraphOptions {
+  template?: TemplateName | Template;
+  orientation?: Orientation;
+  reverseArrow?: boolean;
+  initCommitOffsetX?: number;
+  initCommitOffsetY?: number;
+  mode?: Mode;
+  author?: string;
+  branchLabelOnEveryCommit?: boolean;
+  commitMessage?: string;
+}
 
 export interface OgreGraphProps {
   repository: RepositoryObject<any>;
-  options: GitgraphOptions | undefined;
+  options?: GitgraphOptions;
+  commitOptions?: Map<string, CommitOptions>;
 }
 
 export const OgreGraph: React.FC<OgreGraphProps> = ({
   repository,
   options,
+  commitOptions,
 }) => {
   const [graphData, setGraphData] = useState<any[] | undefined>(undefined);
 
   useEffect(() => {
     if (!graphData) {
       const history = repository.getHistory();
-      setGraphData(formatGit2Json(history));
+      const data = formatGit2Json(history).map((c) => {
+        const opts = commitOptions?.get(c.hash);
+        return {
+          ...c,
+          ...(opts ? opts : {}),
+        };
+      });
+      setGraphData(data);
     }
   }, [repository]);
 
