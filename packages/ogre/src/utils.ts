ee1d091132e47cbe80f1b67d258ab986cf82c724
János Veres <janos@dot.industries>
2024-05-31 13:12:48 +0200

    fix: browser detection for edge runtime support for crypto package

--- packages/ogre/src/utils.ts
+++ packages/ogre/src/utils.ts
@@ -9,6 +9,8 @@ import { RepositoryObject } from "./repository.js";
 const emailRegex =
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
+export const isBrowser = () =>
+  typeof window !== "undefined" && typeof window.document !== undefined;
 export const cleanAuthor = (author: string): [name: string, email: string] => {
   if (author === "") {
     throw new Error(`author not provided`);
